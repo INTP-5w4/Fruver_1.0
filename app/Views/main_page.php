a3ab274a96f1354e5626cfda367b5a4a493a017d
INTP5w4 <joaquinflorescadena2007@gmail.com>
2026-02-21 23:16:21 -0600

    Main page css incompleto

--- app/Views/main_page.php
+++ app/Views/main_page.php
@@ -2,8 +2,8 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Estilos/main_page.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -23,15 +23,26 @@
         </div>
             
     </header>
-    <hero>
-        <h1>FRUVER</h1>
-        <h2>Frutas y verduras de Veracruz</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi dolore ipsa repudiandae voluptatum reprehenderit saepe accusantium assumenda ratione incidunt esse! In nostrum ut inventore quis, molestias sequi consequuntur eos beatae.</p>
-        <a href="<?= base_url('lista_producto')?>"><button>Ver productos</button></a>
-        <div><i class="fa-solid fa-truck-arrow-right"></i> <p>Envio rapido</p></div>
-        <div><i class="fa-regular fa-clock"></i><p>Gran disponibilidad</p></div>
-        <div><i class="fa-solid fa-leaf"></i><p>100% Natural</p></div>
-    </hero>
+    <section class="H_padre">
+        <div class="hero2">
+            <div class="container_h1">
+                <h1>FRU</h1>
+                <h1 class="ver">VER</h1>
+            </div>
+            <h2>Frutas y verduras de Veracruz</h2>
+            <p>Somos una compañia mexicana con una historia damiliar detrás, ofrecemos productos de origen mexicano y alta calidad, los cuales esperamos que degustes y disfrutes.
+             Más que una empresa, somos familia dando lo mejor desde México para México.            </p>
+            <div class="container_button">
+                <a href="<?= base_url('lista_producto')?>"><button>Ver productos</button></a>
+            </div>
+            
+             <div class="iconos_hero">
+                <div><i class="fa-solid fa-truck-arrow-right"></i> <p>Envio rapido</p></div>
+                <div><i class="fa-regular fa-clock"></i><p>Gran disponibilidad</p></div>
+                <div><i class="fa-solid fa-leaf"></i><p>100% Natural</p></div>
+            </div>
+        </div>
+    </section>
     <section>
         <h2>Nuestros Productos</h2>
         <h6>Los mejores precios en frutas, verduras y abarrotes</h6>
@@ -54,7 +65,7 @@
     </section>
     <footer>
         <div class="footer_container">
-            <div class="foot_contenido">
+            <div class="foot_contacto">
                 <h4>Contacto</h4>
                 <p>email: 123456789@gmail.com</p>
                 <p>telefono: +52 12 34 56 78 90</p>
@@ -63,15 +74,16 @@
                 </pre>
             </div>
             
-            <div class="foot_contenido"></div>
-            <h4>Atajos</h4>
-            <ul>
-                <li><a href="#">Pagina de inicio</a></li>
-                <li><a href="#">Sobre nosotros</a></li>
-                <li><a href="#">Productos</a></li>
-            </ul>
+            <div class="foot_atajo">
+                <h4>Atajos</h4>
+                <ul>
+                    <li><a href="#">Pagina de inicio</a></li>
+                    <li><a href="#">Sobre nosotros</a></li>
+                    <li><a href="#">Productos</a></li>
+                </ul>
+            </div>
             
-            <div class="foot_contenido">
+            <div class="foot_redes">
                 <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
                 <li><a href=""><i class="fa-brands fa-facebook"></i></a></li>
                 <li><a href=""><i class="fa-brands fa-whatsapp"></i></a></li>
