dbfdd9cede0358bf5e6a695ee94c3517906392fa
arcos2999-cpu <arcos2999@gmail.com>
2026-02-22 01:30:19 -0600

    control clientes

---
+++ app/Controllers/Clientes.php
@@ -0,0 +1,8 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\Cliente_model;
+class Clientes extends Controller{
+
+}
---
+++ app/Models/Cliente_model.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Cliente_model extends Model{
+    protected $table      = 'cliente';
+    // Uncomment below if you want add primary key
+    // protected $primaryKey = 'id';
+}
