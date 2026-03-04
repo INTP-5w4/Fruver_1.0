4f51103e7afcc4d0512a46de2a291a9ac3d41bef
rubenCode <grodrigoruben@gmail.com>
2026-03-04 17:23:57 -0600

    corregir routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -50,7 +50,6 @@ $routes->get('pasa_id_repartidor/(:num)', 'Repartidores::recupera/$1');
 $routes->post('modifica_repartidor', 'Repartidores::modificar');
 //=============================================================================
 $routes->get('alta_pedido', 'Pedidos::alta_pedido');
-<<<<<<< HEAD
 
 //=============================================================================
 $routes->get('lista_credito', 'Creditos::lista_credito');
@@ -60,6 +59,3 @@ $routes->get('borcre_credito/(:num)', 'Creditos::eliminar_datos/$1');
 $routes->get('pascre_credito/(:num)', 'Creditos::recupera/$1');
 $routes->get('modifica_credito', 'Creditos::modifica_credito');
  
-=======
-$routes->post('Guarda_pedido', 'Pedidos::Guarda_pedido');
->>>>>>> fd9fbefc8b9b2b62d01a1d7da19ca5fcac94e996
