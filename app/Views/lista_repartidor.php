eff6a806b120052db3c852356ad8dfab45f0e995
rubenCode <grodrigoruben@gmail.com>
2026-03-05 11:19:51 -0600

    Ventana Modal añadir repartidor y se agrego un script a crea_repartidor

--- app/Views/lista_repartidor.php
+++ app/Views/lista_repartidor.php
@@ -7,8 +7,12 @@
     <title>Document Title</title>
 </head>
 <body>
-    
-    <table>
+    <button onclick="abreModal('modalRepartidor', '<?= base_url('alta_repartidor') ?>')" 
+    class="w3-button w3-green w3-round-xlarge">
+    Añadir Repartidor
+    </button>
+
+    <table class="w3-table w3-bordered w3-striped w3-hoverable w3-card">
         <thead>
             <tr>
                 <td>ID</td>
@@ -40,6 +44,51 @@
                 }?>
         </tbody>
     </table>
-    <a href="<?= base_url('alta_repartidor') ?>"><button>Añade Repartidor</button></a>
+        <div id="modalRepartidor" class="w3-modal">
+    <div class="w3-modal-content w3-animate-top"
+        style="width:60%; max-width:800px; border-radius:10px; box-shadow:0 8px 25px rgba(0,0,0,0.3);">
+      <header class="w3-container w3-green"style="border-radius:10px 10px 0 0;"> 
+        <span onclick="cierraModal('modalRepartidor')"
+        class="w3-button w3-display-topright">&times;</span>
+        <h2 style="margin:0;">Añadir Repartidor</h2>
+      </header>
+        <div class="w3-container" style="padding:20px;">
+            <iframe id="iframeAdd"
+            style="width:100%; height:450px; border:none;"></iframe>
+        </div>
+      <footer class="w3-container w3-green"
+         style="border-radius:0 0 10px 10px; text-align:right;">
+        <button class="w3-button w3-white" onclick="cierraModal('modalRepartidor')">Cerrar</button>
+    </footer>
+    </div>
+  </div>
+  
+<script>
+    function abreModal(idModal, url) {
+        const modal = document.getElementById(idModal);
+        // Buscamos el iframe que está dentro de ESE modal específico
+        const iframe = modal.querySelector('iframe');
+        
+        iframe.src = url;
+        modal.style.display = "block";
+    }
+
+    function cierraModal(idModal) {
+        const modal = document.getElementById(idModal);
+        const iframe = modal.querySelector('iframe');
+        
+        modal.style.display = "none";
+        iframe.src = "";
+    }
+
+    // Cerrar al hacer clic fuera
+    window.onclick = function(event) {
+        if (event.target.className === 'w3-modal') {
+            event.target.style.display = "none";
+            const iframe = event.target.querySelector('iframe');
+            if(iframe) iframe.src = "";
+        }
+    }
+</script>
 </body>
 </html>
