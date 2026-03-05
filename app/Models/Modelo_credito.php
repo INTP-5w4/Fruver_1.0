94bf51a8c6fbddc6c1b298c64ddf9adddf5984d0
arcos2999-cpu <arcos2999@gmail.com>
2026-03-05 13:03:11 -0600

    errores solucionados rafa

--- app/Models/Modelo_credito.php
+++ app/Models/Modelo_credito.php
@@ -3,24 +3,20 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class Modelo_credito extends Model{
-    protected $table      = 'credito';
-    protected $primaryKey = "id";
-    
-    protected $allowedFields = ['fecha_inicio','fecha_fin','monto','estatus','credito_disponible','cliente_id'];
-    
-    public function getcredito($id){
+class Modelo_Credito extends Model{
+    protected $table      = 'Credito';
+    // Uncomment below if you want add primary key
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['fecha_inicio', 'fecha_fin', 'monto', 'estatus', 'credito_disponible', 'cliente_id'];
+
+    public function getentrada($id){
         return $this->where('id',$id)-> first();
     }
-    
+
     public function borrar($id){
         if($this->delete($id))
             return true;
-            else
-                return false;
-        
+        else
+            return false;
     }
-   
-    // Uncomment below if you want add primary key
-    // protected $primaryKey = 'id';
 }
