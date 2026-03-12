c50053345a533cbbd339e08133b3864fa70eaddc
INTP5w4 <joaquinflorescadena2007@gmail.com>
2026-03-12 13:04:15 -0600

    Parte antes del caos

--- app/Views/header.php
+++ app/Views/header.php
@@ -3,22 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="<?= base_url('Estilos/main_page.css') ?>">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: rgb(225, 225, 225);
+        }
+        header {
+            display: flex;
+            justify-content: space-between;  /* ← space between aquí */
+            align-items: center;
+            padding: 0 30px;
+            height: 40px;
+            width: 100%;
+            background-color: rgb(225, 225, 225);
+        }
+        nav a {
+            margin-left: 20px;
+            font-size: 0.85em;
+        }
+    </style>
+    <title>Header</title>
 </head>
 <body>
     <header>
-        <div class="container">
-            <p class="logo">FRUVER</p>
-            <nav>
-                <a href="#">Sobre nosotros</a>
-                <a href="#">Productos</a>
-                <a href="#">Contacto</a>
-                </div>
-                
-                    
-            </nav>       
-        </div>
-            
+        <p class="logo">FRUVER</p>
+        <nav>
+            <a href="#">Sobre nosotros</a>
+            <a href="#">Productos</a>
+            <a href="#">Contacto</a>
+        </nav>
     </header>
 </body>
 </html>
--- app/Views/main_page.php
+++ app/Views/main_page.php
@@ -6,10 +6,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('Estilos/main_page.css') ?>">
-    <title>Página de Inicio2</title>
+    <title>Página de Inicio</title>
 </head>
 <body>
-    <iframe src="<?= base_url('header') ?>" class="header_frame"></iframe>
+    <?php #===================================================================== ?>
+    <iframe src="<?= base_url('header') ?>" 
+        style="height:40px; width:100%; border:none; display:block;">
+</iframe>
+<?php #===================================================================== ?>
     <section class="H_padre">
         <div class="hero2">
             <div class="container_h1">
